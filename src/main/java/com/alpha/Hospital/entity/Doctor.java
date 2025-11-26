aacae8f87915d87c135d4dde40fa0014ca6fb757
aadityasankalp2125-beep <aadityasankalp.2125@gmail.com>
2025-11-26 19:05:33 +0530

    ADDED Doctor entity

---
+++ src/main/java/com/alpha/Hospital/entity/Doctor.java
@@ -0,0 +1,58 @@
+package com.alpha.Hospital.entity;
+
+import java.util.List;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.OneToMany;
+
+@Entity
+public class Doctor {
+	private int id;
+	private String name;
+	private String specialization;
+	@OneToMany(cascade = CascadeType.ALL)
+	private List<Patient>plist;
+	@Override
+	public String toString() {
+		return "Doctor [id=" + id + ", name=" + name + ", specialization=" + specialization + ", plist=" + plist + "]";
+	}
+	public Doctor(int id, String name, String specialization, List<Patient> plist) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.specialization = specialization;
+		this.plist = plist;
+	}
+	
+	public Doctor() {
+		super();
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getSpecialization() {
+		return specialization;
+	}
+	public void setSpecialization(String specialization) {
+		this.specialization = specialization;
+	}
+	public List<Patient> getPlist() {
+		return plist;
+	}
+	public void setPlist(List<Patient> plist) {
+		this.plist = plist;
+	}
+	
+	
+
+}
