30ad841bbd710ce120cb303378df37a3e44c6add
= <=>
2025-11-26 19:27:47 +0530

    paitent entity created

--- src/main/java/com/alpha/Hospital/entity/Patient.java
+++ src/main/java/com/alpha/Hospital/entity/Patient.java
@@ -1,5 +1,44 @@
 package com.alpha.Hospital.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity
 public class Patient {
+	@Id
+	private int id;
+	private String name;
+	private String diseases;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getDiseases() {
+		return diseases;
+	}
+	public void setDiseases(String diseases) {
+		this.diseases = diseases;
+	}
+	@Override
+	public String toString() {
+		return "Patient [id=" + id + ", name=" + name + ", diseases=" + diseases + "]";
+	}
+	public Patient() {
+		super();
+	}
+	public Patient(int id, String name, String diseases) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.diseases = diseases;
+	}
 
 }
